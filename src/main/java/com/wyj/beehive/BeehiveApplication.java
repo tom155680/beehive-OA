6d46909aaea09cbdc9741207ba77cec017be9db7
wyj <1556802726@qq.com>
2023-03-25 21:11:36 +0800

    Initial commit,create project

---
+++ src/main/java/com/wyj/beehive/BeehiveApplication.java
@@ -0,0 +1,13 @@
+package com.wyj.beehive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeehiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeehiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyj/beehive/BeehiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyj.beehive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeehiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
